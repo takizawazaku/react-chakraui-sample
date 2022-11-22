6eb9e82d54d425848dead6aca5cb392ba8a9c235
E17 <E17@e-mall.co.jp>
2022-11-22 13:01:36 +0900

    その他サンプルに各ステータスごとのボタンを追加

--- src/components/pages/home/SampleEtc.tsx
+++ src/components/pages/home/SampleEtc.tsx
@@ -4,18 +4,48 @@ import { useMessage } from "../../../hooks/useMessage";
 import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 
 export const SampleEtc: VFC = memo(() => {
+  // コンポーネントは後でバラすべきか？
   const { showMessage } = useMessage();
-  const onClickToast = () => {
+  const onClickToast = (status: "info" | "warning" | "success" | "error") => {
+    let title = "";
+    switch (status) {
+      case "info":
+        title = "情報";
+        break;
+      case "warning":
+        title = "警告";
+        break;
+      case "success":
+        title = "成功";
+        break;
+      case "error":
+        title = "失敗";
+        break;
+      default:
+        title = "";
+        break;
+    }
     showMessage({
-      title: "トーストテスト",
-      status: "info",
+      title,
+      status,
       variant: "top-accent",
     });
   };
 
   return (
     <>
-      <PrimaryButton onClick={onClickToast}>トースト表示</PrimaryButton>
+      <PrimaryButton onClick={() => onClickToast("info")}>
+        トースト表示(info)
+      </PrimaryButton>
+      <PrimaryButton onClick={() => onClickToast("warning")}>
+        トースト表示(warning)
+      </PrimaryButton>
+      <PrimaryButton onClick={() => onClickToast("success")}>
+        トースト表示(success)
+      </PrimaryButton>
+      <PrimaryButton onClick={() => onClickToast("error")}>
+        トースト表示(error)
+      </PrimaryButton>
     </>
   );
 });
