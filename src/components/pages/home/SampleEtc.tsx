9fa00cf1ba04090e743d1f65a2d23555aa6142c2
E17 <E17@e-mall.co.jp>
2022-11-22 09:08:29 +0900

    その他サンプル　トースト表示ボタン追加

--- src/components/pages/home/SampleEtc.tsx
+++ src/components/pages/home/SampleEtc.tsx
@@ -1,5 +1,17 @@
+import { Button } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { useMessage } from "../../../hooks/useMessage";
+import { PrimaryButton } from "../../atoms/button/PrimaryButton";
 
 export const SampleEtc: VFC = memo(() => {
-  return <p>色々雑に試すためのサンプルです</p>;
+  const { showMessage } = useMessage();
+  const onClickToast = () => {
+    showMessage({ title: "aaaa", status: "info" });
+  };
+
+  return (
+    <>
+      <PrimaryButton onClick={onClickToast}>トースト表示</PrimaryButton>
+    </>
+  );
 });
